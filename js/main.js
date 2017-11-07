579007250d29e1133cbf06db6b441586b7c90997
Karl Veskus <karl.veskus@gmail.com>
2017-11-07 05:50:29 +0200

    Add knockout.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,4 @@
-const openSidebar = document.getElementById('openSidebar');
-openSidebar.addEventListener('click', function() {
-    document.getElementById("sidebar").style.width = "250px";
-})
-
-const closeSidebar = document.getElementById('closeSidebar');
-closeSidebar.addEventListener('click', function() {
-    document.getElementById("sidebar").style.width = "0";
-})
-
-var map;
+let map;
 
 function initMap() {
     // Tartu location
@@ -21,3 +11,30 @@ function initMap() {
         mapTypeControl: false
     });
 }
+
+var Restaurant = function(data) {
+    this.name = data.name;
+    this.location = data.location;
+}
+
+var ViewModel = function() {
+    let self = this;
+    this.restaurantList = ko.observableArray([]);
+
+    restaurants.forEach(function(restaurantItem) {
+        console.log(restaurantItem)
+        self.restaurantList.push( new Restaurant(restaurantItem) );
+    });
+
+    self.openSidebar = function() {
+        document.getElementById("sidebar").style.width = "250px";
+    }
+
+    self.closeSidebar = function() {
+        document.getElementById("sidebar").style.width = "0";
+    }
+}
+
+
+var viewModel = new ViewModel()
+ko.applyBindings(viewModel);
---
+++ restaurants.js
@@ -0,0 +1,10 @@
+var restaurants = [
+    {
+        'name': 'Taverna',
+        'location': {lat: 58.3797654, lng: 26.7229806}
+    },
+    {
+        'name': 'Vapiano',
+        'location': {lat: 58.3794454, lng: 26.7195045}
+    }
+]
