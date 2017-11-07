b8e394015fd020c964ece6dc4cc6b7e41609b0a4
Karl Veskus <karl.veskus@gmail.com>
2017-11-07 03:16:24 +0200

    Add Google Maps support

--- js/main.js
+++ js/main.js
@@ -7,3 +7,17 @@ const closeSidebar = document.getElementById('closeSidebar');
 closeSidebar.addEventListener('click', function() {
     document.getElementById("sidebar").style.width = "0";
 })
+
+var map;
+
+function initMap() {
+    // Tartu location
+    const tartu = {lat: 58.3776, lng: 26.7290};
+
+    // Constructor creates a new map
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: tartu,
+        zoom: 14,
+        mapTypeControl: false
+    });
+}
