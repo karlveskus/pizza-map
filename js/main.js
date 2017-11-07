1500c8063b25b0975dfd38a43204a0338681befe
Karl Veskus <karl.veskus@gmail.com>
2017-11-07 06:26:58 +0200

    Add restaurant filtering

--- js/main.js
+++ js/main.js
@@ -15,16 +15,35 @@ function initMap() {
 var Restaurant = function(data) {
     this.name = data.name;
     this.location = data.location;
+    this.visible = ko.observable(true);
 }
 
 var ViewModel = function() {
     let self = this;
     this.restaurantList = ko.observableArray([]);
+    this.filter = ko.observable("");
 
     restaurants.forEach(function(restaurantItem) {
-        console.log(restaurantItem)
         self.restaurantList.push( new Restaurant(restaurantItem) );
     });
+
+    this.filteredRestaurantList = ko.computed( function() {
+		var filterLower = self.filter().toLowerCase();
+		if (filterLower) {
+            return ko.utils.arrayFilter(self.restaurantList(), function(restaurantItem) {
+				var nameLower = restaurantItem.name.toLowerCase();
+                var isMatch = (nameLower.search(filterLower) >= 0);
+                
+				restaurantItem.visible(isMatch);
+				return isMatch;
+			});
+		} else {
+			self.restaurantList().forEach(function(restaurantItem){
+				restaurantItem.visible(true);
+			});
+			return self.restaurantList();
+		}
+	});
 
     self.openSidebar = function() {
         document.getElementById("sidebar").style.width = "250px";
@@ -33,6 +52,10 @@ var ViewModel = function() {
     self.closeSidebar = function() {
         document.getElementById("sidebar").style.width = "0";
     }
+
+
+
+    
 }
 
 
