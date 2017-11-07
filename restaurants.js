a5ab7a8f9138727b8aca4c5c7aa1d2812238b3e1
Karl Veskus <karl.veskus@gmail.com>
2017-11-07 08:24:28 +0200

    Add more pizza restaurants

--- restaurants.js
+++ restaurants.js
@@ -1,10 +1,42 @@
 var restaurants = [
     {
         'name': 'Taverna',
-        'location': {lat: 58.3797654, lng: 26.7229806}
+        'location': {lat: 58.380693, lng: 26.724596}
     },
     {
         'name': 'Vapiano',
-        'location': {lat: 58.3794454, lng: 26.7195045}
+        'location': {lat: 58.376915, lng: 26.728028}
+    },
+    {
+        'name': 'Pizzeria La Dolce Vita',
+        'location': {lat: 58.381973, lng: 26.722757}
+    },
+    {
+        'name': 'Ruunipizza ja Pannkoogikohvik',
+        'location': {lat: 58.380530, lng: 26.722059}
+    },
+    {
+        'name': 'Pappa Pizza',
+        'location': {lat: 58.375007, lng: 26.724068}
+    },
+    {
+        'name': 'Opera Pizza',
+        'location': {lat: 58.375334, lng: 26.720571}
+    },
+    {
+        'name': 'Pizzapoisid Tartu',
+        'location': {lat: 58.383436, lng: 26.737161}
+    },
+    {
+        'name': 'Tartu Ujula Pizzakiosk',
+        'location': {lat: 58.386277, lng: 26.723929}
+    },
+    {
+        'name': 'Kebab Pizza OÜ',
+        'location': {lat: 58.388125, lng: 26.727662}
+    },
+    {
+        'name': 'Pizzapood',
+        'location': {lat: 58.368136, lng: 26.721058}
     }
 ]
