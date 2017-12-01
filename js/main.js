6259744e2bcb4a95033593d671053086b6ebce4a
Karl Veskus <karl.veskus@gmail.com>
2017-12-01 20:40:59 +0200

    Fix #2, limit infowindows to 1

--- js/main.js
+++ js/main.js
@@ -1,68 +1,73 @@
 let map;
+let infoWindow;
 
 const Restaurant = function(venue) {
     const self = this;
     this.visible = ko.observable(true);
     this.name = venue.name;
     this.location = venue.location;
-
-    // Build a nice content to show in InfoWindow
-    let infoWindowContent = '<div><b>' + this.name + '</b></div><hr>';
-    if (venue.hasOwnProperty('rating')) {
-        infoWindowContent += '<div><b>Rating:</b> ' + venue.rating + '/10</div>';
-    }
-    if (venue.hasOwnProperty('url')) {
-        infoWindowContent += '<div><b>Link:</b> <a target="_blank" href="' + venue.url + '">' + venue.url + '</a></div>';
-    }
-    if (venue.hasOwnProperty('contact') && venue.contact.hasOwnProperty('formattedPhone')) {
-        infoWindowContent += '<div><b>Phone:</b> ' + venue.contact.formattedPhone + '</div>';
-    }
-    if (venue.hasOwnProperty('popular') && venue.popular.hasOwnProperty('status')) {
-        infoWindowContent += '<div><b>Status:</b> ' + venue.popular.status + '</div>';
-    }
-    
-    this.infoWindow = new google.maps.InfoWindow({content: infoWindowContent});
     
     this.marker = new google.maps.Marker({
         position: new google.maps.LatLng(this.location.lat, this.location.lng),
         map: map,
-        title: this.name
+        title: this.name,
     });
 
     // Hide and show markers on the map
-    this.showRestaurantMarker = ko.computed(function() {
+    this.showRestaurantMarker = ko.computed(() => {
         if (this.visible() === true) {
             this.marker.setMap(map);
         } else {
             this.marker.setMap(null);
         }
-    }, this);
+    });
 
     // Click event listeners for markers
     this.marker.addListener('click', function() {
-        self.infoWindow.open(map, this);
+        if (infoWindow) {
+            infoWindow.close();
+        }
+
+        // Build a nice content to show in InfoWindow
+        let infoWindowContent = '<div><b>' + self.name + '</b></div><hr>';
+        if (venue.hasOwnProperty('rating')) {
+            infoWindowContent += '<div><b>Rating:</b> ' + venue.rating + '/10</div>';
+        }
+        if (venue.hasOwnProperty('url')) {
+            infoWindowContent += '<div><b>Link:</b> <a target="_blank" href="' + venue.url + '">' + venue.url + '</a></div>';
+        }
+        if (venue.hasOwnProperty('contact') && venue.contact.hasOwnProperty('formattedPhone')) {
+            infoWindowContent += '<div><b>Phone:</b> ' + venue.contact.formattedPhone + '</div>';
+        }
+        if (venue.hasOwnProperty('popular') && venue.popular.hasOwnProperty('status')) {
+            infoWindowContent += '<div><b>Status:</b> ' + venue.popular.status + '</div>';
+        }
+
+        infoWindow = new google.maps.InfoWindow({content: infoWindowContent});
+
+        infoWindow.open(map, this);
         
-		self.marker.setAnimation(google.maps.Animation.BOUNCE);
-      	setTimeout(function() {
-      		self.marker.setAnimation(null);
-     	}, 1400);
+        self.marker.setAnimation(google.maps.Animation.BOUNCE);
+        setTimeout(function() {
+            self.marker.setAnimation(null);
+        }, 1400);
     });
     
     this.bounce = function(place) {
-		google.maps.event.trigger(self.marker, 'click');
+        google.maps.event.trigger(self.marker, 'click');
     };
 };
 
 const ViewModel = function() {
     const self = this;
     this.restaurantList = ko.observableArray([]);
-    this.filter = ko.observable("");
+    this.filter = ko.observable('');
 
     // Generate a map
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 58.3776, lng: 26.7290},
         zoom: 14,
-        mapTypeControl: false
+        mapTypeControl: false,
     });
     
     // Make Restaurants from Foursquare VENUE_IDs
